c42fe5d05cc464705a35127a860fab302da47dc4
Fabian.trllr <33369156+Exo-poulpe@users.noreply.github.com>
2018-12-13 13:37:41 +0100

    Push solution to project

---
+++ BigData/BigFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BigData
+{
+    class BigFile
+    {
+
+        string _path = string.Empty;
+
+        public string Path { get => _path; private set => _path = value; }
+        
+        public BigFile(string path)
+        {
+            this.Path = path;
+        }
+
+        public int TotalLines()
+        {
+            return File.ReadAllLines(this.Path).Length;
+        }
+
+    }
+}
---
+++ BigData/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Orient.Client;
+using OrientDB_Net.binary.Innov8tive.API;
+
+namespace BigData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Count;
+        }
+
+        public void Connect(object sender,EventArgs e)
+        {
+            ConnectionOptions test = new ConnectionOptions();
+            test.DatabaseName = "demodb";
+            test.HostName = "10.5.51.31";
+            test.Port = 2480;
+            test.UserName = "admin";
+            test.Password = "admin";
+            Application.Exit();
+            ODatabase t = new ODatabase(test);
+            Console.WriteLine(t.DatabaseProperties);
+        }
+
+        public void Count(object sender,EventArgs e)
+        {
+            BigFile bf = new BigFile(@"C:\Users\Administrateur\Desktop\crackstation.txt");
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(bf.TotalLines());
+        }
+        
+
+
+    }
+}
