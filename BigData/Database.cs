8b23dde0feb603cc86c95291b31e9da0caff88f9
Fabian.trllr <33369156+Exo-poulpe@users.noreply.github.com>
2018-12-20 13:05:50 +0100

    Add connect exemple

--- BigData/Database.cs
+++ BigData/Database.cs
@@ -18,12 +18,28 @@ namespace BigData
         private static int _port = 2480;
         private static string _userName = "admin";
         private static string _passWord = "admin";
-        
 
+        public static ConnectionOptions Db { get => _db; set => _db = value; }
+        public static string NameDatabase { get => _nameDatabase; set => _nameDatabase = value; }
+        public static string HostName { get => _hostName; set => _hostName = value; }
+        public static int Port { get => _port; set => _port = value; }
+        public static string UserName { get => _userName; set => _userName = value; }
+        public static string PassWord { get => _passWord; set => _passWord = value; }
 
         public static void Connect()
         {
+            Db.DatabaseName = NameDatabase;
+            Db.HostName = HostName;
+            Db.Port = Port;
+            Db.UserName = UserName;
+            Db.Password = PassWord;
 
+            var database = new ODatabase(Db); 
+        }
+
+        public static void InsertPassword(string password)
+        {
+            
         }
 
     }
